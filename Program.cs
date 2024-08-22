beb967f2d9df2cf19e9d13a0628b936e2f7ed917
Dunya-coder <diloromabdusattorova90@gmail.com>
2024-08-22 15:19:42 +0500

    palindrom_text

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace vscode
+{
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("write word:");
+        string text = Console.ReadLine();
+        bool palindrom = false;
+        char[] check = text.ToArray();
+        int length = check.Length;
+        for (int i = 0; i <= length / 2; i++)
+        {
+            
+                if (check[length-i-1] == check[i])
+                {
+                    palindrom = true;
+                }
+                else 
+                {
+                    palindrom=false;
+                    break;
+                }
+            
+        }
+        if (palindrom == true)
+        {
+            System.Console.WriteLine("palindrom");
+        }
+        if(palindrom==false)
+        {
+            System.Console.WriteLine("palindrom emas");
+        }
+    }
+}
+}
